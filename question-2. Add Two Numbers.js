2c532ad3914b92f0560ae172e3291ae48389ed72
alan <q06637557832@gmail.com>
2020-06-09 01:39:10 +0800

    There are two sorted arrays nums1 and nums2 of size m and n respectively.
    Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
    
    You may assume nums1 and nums2 cannot be both empty.

--- question-2. Add Two Numbers.js
+++ question-2. Add Two Numbers.js
@@ -3,17 +3,33 @@
 //Output: 7 -> 0 -> 8
 //Explanation: 342 + 465 = 807.
 
-var a = [2,4,3];
-var b = [5,6,4];
-var reversed = a.reverse();
-var reversed = b.reverse();
-var c= [a[0]+b[0],a[1]+b[1],a[2]+b[2]];
-if (Math.ceil(c[1])){
-    c[0]=c[0]+1;
-    c[1]=c[1]%10;
-    c.reverse();
-}
+var addTwoNumbers = function(l1, l2) {
+    var list = new ListNode(0);
+    var result = list;
 
-console.log(a);
-console.log(b);
-console.log(c);
+    add(l1,l2,0);
+    return result.next;
+
+    function add(l1,l2,gap){
+        var sum = 0;
+        if(l1 === null && l2 === null && gap === 0){
+            return 0;
+        }
+
+        if(l1 !== null){
+            sum += l1.val;
+            l1 = l1.next;
+        }
+
+        if(l2 !== null){
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        sum += gap;
+        list.next = new ListNode(sum%10);
+        gap = parseInt(sum/10);
+        list = list.next;
+        add(l1,l2,gap);
+    }
+};
+addTwoNumbers(l1, l2);
